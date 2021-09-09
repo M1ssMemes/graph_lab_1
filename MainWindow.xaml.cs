5a2af9d94564d3ab4f7b9696748333b8d32b4dd9
MEMES <fv-765@mail.ru>
2021-09-09 22:42:02 +0300

    f

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,122 +1,150 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Graph_Lab_1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        double x1 = 0;
-        double x2 = 0;
-        double y1 = 0;
-        double y2 = 0;
+        Point first;
+        Point last;
 
+        SolidColorBrush color = new SolidColorBrush();
         bool isPressed = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            color.Color = Colors.Black;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ExitButtonClick(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
-        private void Canva_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void DefineFirstPoint(object sender, MouseButtonEventArgs e)
         {
-
-                Canva.Children.Clear();
-                isPressed = true;
-                Point p = e.GetPosition(this);
-                x1 = Convert.ToInt32(p.X);
-                y1 = Convert.ToInt32(p.Y);
-
+            Canva.Children.Clear();
+            isPressed = true;
+            first = e.GetPosition(this);
         }
 
-
-        private void Canva_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void DrawLineToPoint(object sender, MouseButtonEventArgs e)
         {
             if (isPressed)
-            { 
-                Point p = e.GetPosition(this);
-                x2 = Convert.ToInt32(p.X);
-                y2 = Convert.ToInt32(p.Y);
+            {
+                last = e.GetPosition(this);
 
-                SolidColorBrush color = new SolidColorBrush();
-                color.Color = Colors.Black;
+                var str = Data.Text;
+                var numb = Razdel(str);
 
-                CreateLine(x1, y1, x2, y2, color);
-
-                x1 = 0;
-                x2 = 0;
-                y1 = 0;
-                y2 = 0;
+                DrawLine(first, last, numb);
 
                 isPressed = false;
             }
         }
-     
 
-        public void CreateLine(double x1, double y1, double x2, double y2, SolidColorBrush color)
+        public void DrawLine(Point first, Point last, List<int> numb)
         {
-            Point point = new Point(GetCoord(x1, y1, x2, y2));
+
+            var coords = GetCoord(first, last).AlternateElements(numb);
+            SolidColorBrush brushcolor = new SolidColorBrush(Colors.Black);
 
 
+            foreach (var coor in coords)
+            {
+
+                var x = coor.Item1;
+                var y = coor.Item2;
+                Rectangle rec = new Rectangle();
+                Canvas.SetTop(rec, y);
+                Canvas.SetLeft(rec, x);
+
+                rec.Width = 1;
+                rec.Height = 1;
+                rec.Fill = brushcolor;
+
+                Canva.Children.Add(rec);
+            }
             return;
         }
 
-
-
-
-        public IEnumerable<Tuple<double, double>> GetCoord(double x1, double y1, double x2, double y2)
-          {
-            double dx = Math.Abs(x2 - x1);
-            double dy = -Math.Abs(y2 - y1);
-
-            double sx = x1 < x2 ? 1 : -1;
-            double sy = y1 < y2 ? 1 : -1;
-
-            double err = dx + dy;
-            double e2;
-              while(true)
-                {
-                    yield return Tuple.Create(x1, y1);
-
-                    if (x1 == x2 && y1 == y2) break;
-
-                    e2 = 2 * err;
-
-                    if (e2 > dy)
-                    {
-                        err += dy;
-                        x1 += sx;
-                    }
-                    else if (e2 < dx)
-                    { 
-                        err += dx;
-                        y1 += sy;
-                    }
-                }
+        public List<int> Razdel(string str)
+        {
+            return str.Split().Select(i => int.Parse(i)).ToList();
         }
-            
-         }
- 
 
+        public IEnumerable<Tuple<double, double>> GetCoord(Point first, Point last)
+        {
+            var x1 = first.X;
+            var y1 = first.Y;
+            var x2 = last.X;
+            var y2 = last.Y;
 
+            var sx = x1 < x2 ? 1 : -1;
+            var sy = y1 < y2 ? 1 : -1;
 
+            var dx = Math.Abs(x2 - x1);
+            var dy = -Math.Abs(y2 - y1);
+            var err = dx + dy;
+
+            while (true)
+            {
+                yield return Tuple.Create(x1, y1);
+
+                if (x1 == x2 && y1 == y2) break;
+
+                var e2 = 2 * err;
+
+                if (e2 > dy)
+                {
+                    err += dy;
+                    x1 += sx;
+                }
+                else
+                {
+                    err += dx;
+                    y1 += sy;
+                }
+            }
+        }
+    }
+
+    static class EnumExtentions
+    {
+        public static IEnumerable<T> AlternateElements<T>(this IEnumerable<T> source, List<int> num)
+        {
+
+            var i = 0;
+            var k = num[i];
+            var paint = true;
+            foreach (var element in source)
+            {
+                if (k > 0)
+                {
+                    k--;
+                    if (paint)
+                        yield return element;
+                    else
+                        continue;
+                }
+                else
+                {
+                    i++;
+
+                    if (i == num.Count)
+                        i = 0;
+
+                    k = num[i];
+                    paint = !paint;
+                }
+            }
+        }
     }
 }
